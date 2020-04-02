f7e16cb1d178fe69ad635e6bf37bb9ffa33cd083
Astitva Srivastava <30767291+AstitvaSri@users.noreply.github.com>
2020-04-02 12:01:21 +0530

    Add files via upload

---
+++ src/utils/SVM_input.py
@@ -0,0 +1,99 @@
+import sklearn.cluster as cluster
+import detect_upper_body as ubody
+import cv2
+import sys
+
+def create_codebook(path):
+    orb = cv2.ORB_create()
+    dataset_path = ""
+    images = []
+    features  = []
+    for image in images:
+        boxes = ubody.get_upper_bodies(image)
+        for box in boxes:
+            kp, des = orb.detectAndCompute(box,None)
+            if des is not None:
+                features.extend(des)
+    kmeans = cluster.KMeans(n_clusters = 400, n_init=10)
+    kmeans.fit(features)
+    codebook = kmeans.cluster_centers_
+    #save codebook to disk
+   
+    
+
+def divide_regions(image):
+
+    height, width = image.shape[:2]
+
+    #dividing into 2x2 sub-regions
+
+    #1
+    start_row, start_col = int(0), int(0)
+    end_row, end_col = int(height *.5), int(width*.5)
+    region_1 = image[start_row:end_row , start_col:end_col]
+
+    #2
+    start_row, start_col = int(0), int(width * .5)
+    end_row, end_col = int(height*.5), int(width)
+    region_2 = image[start_row:end_row , start_col:end_col]
+
+    #3
+    start_row, start_col = int(height*.5), int(0)
+    end_row, end_col = int(height), int(width*.5)
+    region_3 = image[start_row:end_row , start_col:end_col]
+
+    #4
+    start_row, start_col = int(height *.5), int(width*.5)
+    end_row, end_col = int(height), int(width)
+    region_4 = image[start_row:end_row , start_col:end_col]
+
+    return region_1,region_2,region_3,region_4
+
+
+
+def get_region_feature(des,codebook):
+    #knn
+    hist = np.zeros(400)
+    for d in des:
+        count = 0
+        min_count = 0
+        histogram = []    
+        min_dist = sys.maxsize
+        for word in codebook:
+            dist = np.linalg.norm(word-np.array(d),2)
+            if dist<min_dist:
+                min_count = count
+                min_dist = dist
+            count += 1
+        hist[min_count] += 1
+    return hist
+
+
+def extract_inpput_features(path):
+    orb = cv2.ORB_create()
+    dataset_path = ""
+    images = []
+    codebook = None #load from disk    
+    INPUT_TO_SVM = []
+    #extracting features from each sub-region
+    for image in images:
+        boxes = ubody.get_upper_bodies(image)
+        this_image_features = []
+        for box in boxes:
+            regions = divide_regions(box)
+            this_box_feature  = []
+            for region in regions:
+                kp, des = orb.detectAndCompute(region,None)
+                if des is not None:
+                    subfeature = get_region_feature(des,codebook)
+                    this_box_feature.extend(subfeature)
+            concatenated_bounding_box_feature = np.hstack([this_box_feature[0],this_box_feature[1],this_box_feature[2],this_box_feature[3]])
+            this_image_features.extend(this_box_feature)
+        INPUT_TO_SVM.extend(this_image_features)
+        return INPUT_TO_SVM
+    
+        
+            
+
+    
+
---
+++ src/utils/detect_upper_body.py
@@ -0,0 +1,70 @@
+import cv2
+import imutils
+import matplotlib.pyplot as plt
+
+def get_upper_bodies(image):
+    haar_upper_body_cascade = cv2.CascadeClassifier("src/utils/haarcascade_upperbody.xml") 
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert video to grayscale
+
+    upper_body = haar_upper_body_cascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.1,
+        minNeighbors = 5,
+        minSize = (50, 100), # Min size for valid detection, changes according to video size or body size in the video.
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the upper bodies
+    ubodies = []
+    for (x, y, w, h) in upper_body:
+        crop_img = image[y:y+h, x:x+w]
+        ubodies.append(crop_img)
+
+    return ubodies
+            
+###########################################################################################
+###########################################################################################
+
+def real_time_upper_body():
+
+    haar_upper_body_cascade = cv2.CascadeClassifier("src/utils/haarcascade_upperbody.xml")
+
+    # Uncomment this for real-time webcam detection
+    # video_capture = cv2.VideoCapture(0)
+
+    # For real-time sample video detection
+    video_capture = cv2.VideoCapture("data/subway.mp4")
+    video_width = video_capture.get(3)
+    video_height = video_capture.get(4)
+
+    while True:
+        ret, frame = video_capture.read()
+
+        frame = imutils.resize(frame, width=1000) # resize original video for better viewing performance
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert video to grayscale
+
+        upper_body = haar_upper_body_cascade.detectMultiScale(
+            gray,
+            scaleFactor = 1.1,
+            minNeighbors = 5,
+            minSize = (50, 100), # Min size for valid detection, changes according to video size or body size in the video.
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the upper bodies
+        for (x, y, w, h) in upper_body:
+            crop_img = frame[y:y+h, x:x+w]
+            plt.imshow(crop_img)
+            plt.show()
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1) # creates green color rectangle with a thickness size of 1
+            cv2.putText(frame, "Upper Body Detected", (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2) # creates green color text with text size of 0.5 & thickness size of 2
+        cv2.imshow('Video', frame) # Display video
+
+        # stop script when "q" key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release capture
+    video_capture.release()
+    cv2.destroyAllWindows()
